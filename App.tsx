f06b1f70b615c51dc4bde016df5b5bcc6e35e335
Juan Torreal <juantorreal@gmail.com>
2022-07-05 19:51:36 -0300

    app structure

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Welcome } from './src/Components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
+
+export default function App() {
+  return (
+      <Dashboard/>
+  );
+}
+
+
