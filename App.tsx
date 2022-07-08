f562771cabe85c9bf0b65e551418360b65479bea
Juan Torreal <juantorreal@gmail.com>
2022-07-08 19:42:06 -0300

    styles

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
-
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/Components/Welcome';
 import { Dashboard } from './src/screens/Dashboard';
+import { ThemeProvider } from 'styled-components';
+import theme from "./src/global/styles/theme"
+import { 
+  useFonts, 
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+        
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+    
+   if(!fontsLoaded){
+    return 
+    <AppLoading/>
+   }
+
   return (
-      <Dashboard/>
+    <ThemeProvider theme={theme}>
+     <Dashboard/>
+    </ThemeProvider>
+      
   );
 }
 
