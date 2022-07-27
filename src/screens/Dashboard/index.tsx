d0210bf9bfa7132d62cb18fe23149344df705b02
Juan Torreal <juantorreal@gmail.com>
2022-07-26 21:56:16 -0300

    TransactionLit

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,6 +10,9 @@ import {
     UserGreeting,
     UserName,
     HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 
 
 
@@ -17,8 +20,18 @@ import {
 } from "./styles";
 import { RFValue } from "react-native-responsive-fontsize";
 import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
 export function Dashboard() {
+
+    const data = [{ 
+        title:"Desenvolvimento de site",
+    amount:"1btc",
+    category:{
+        name: 'vendas',
+        icon: 'dollar-sign'
+    },
+    date:"12/12/21"}]
     return (
         <Container>
             <Header>
@@ -36,10 +49,32 @@ export function Dashboard() {
             </Header>
             <HighlightCards 
              >
-             <HighlightCard/>
-             <HighlightCard/>
-             <HighlightCard/>
+             <HighlightCard title="Entradas" 
+             amount="R$17.400,00"
+              lastTransaction="Última entrada dia 13 de Abril"
+              type="up" />
+              <HighlightCard title="Saídas" 
+             amount="R$7.400,00"
+              lastTransaction="Última saída dia 13 de Abril"
+              type= "down"/>
+              <HighlightCard title="Total" 
+             amount="R$10.000,00"
+              lastTransaction="01 à 13 de abril "
+              type= "total"/>
+             
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+                <TransactionList data={data}
+                renderItem={({item}) => <TransactionCard data={item}}
+                />
+
+
+                
+                />
+
+            </Transactions>
            
         </Container>
     )
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View `
  flex: 1;
@@ -14,7 +15,8 @@ export const Header = styled.View`
  height: ${RFPercentage(42)}px;
  background-color: ${({theme})=> theme.colors.primary };
  justify-content: center;
- align-items: center;
+ align-items: flex-start;
+ flex-direction: row;
 
 
 `;
@@ -22,6 +24,7 @@ export const Header = styled.View`
 export const UserWrapper = styled.View` 
   width: 100%;
   padding: 0 24px;
+  margin-top: ${getStatusBarHeight() + RFValue(28)}px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -62,4 +65,22 @@ align-items: center;
     horizontal: true,
      showsHorizontalScrollIndicator:false,
             contentContainerStyle:{paddingHorizontal: 24}
-   })` `;
+   })`
+   width: 100%;
+   position: absolute;
+   margin-top: ${RFPercentage(20)}px;
+`;
+
+   export const Transactions = styled.View`
+   flex: 1%;
+   padding: 0 24px
+   margin-top: ${RFPercentage(12)}px;
+   `;
+
+   export const Title = styled.Text`
+   font-size: ${RFValue(18)}px;
+   font-family: ${({theme}) => theme.fonts.regular};
+   margin-bottom:  16px;
+   `;
+   
+   export const TransactionList = styled.FlatList` `;
